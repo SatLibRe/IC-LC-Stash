a6b2c5c0a806d09fb17668d86f5596a192ba2e29
Miles Higbie <miles.higbie@gmail.com>
2020-04-23 10:20:01 -0400

    first

---
+++ repLeet.js
@@ -0,0 +1,56 @@
+const meetingTimes = [
+    { startTime: 0,  endTime: 1 },
+    { startTime: 3,  endTime: 5 },
+    { startTime: 4,  endTime: 8 },
+    { startTime: 10, endTime: 12 },
+    { startTime: 9,  endTime: 10 },
+  ]
+
+//   [
+//     { startTime: 0, endTime: 1 },
+//     { startTime: 3, endTime: 8 },
+//     { startTime: 9, endTime: 12 },
+//   ]
+
+const merger = (meetingTimes) => {
+    const sortedMeetings = meetingTimes.sort((a,b) => a.startTime - b.startTime)
+    const mergedMeetings = [sortedMeetings[0]]
+
+    for (let i = 1; i < sortedMeetings.length; i++) {
+        let currentMeeting = sortedMeetings[i]
+        let lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1]
+
+        if(currentMeeting.startTime <= lastMergedMeeting.endTime){
+            lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime)
+        } else {
+            mergedMeetings.push(currentMeeting)
+        }
+        
+    }
+    console.log(mergedMeetings)
+}
+
+merger(meetingTimes)
+
+
+
+/////mutable array 
+
+
+
+function reverse(arrayOfChars) {
+    let leftIndex = 0
+    let rightIndex = arrayOfChars.length - 1
+
+    while(leftIndex < rightIndex){
+        let temp = arrayOfChars[leftIndex]
+        arrayOfChars[leftIndex] = arrayOfChars[rightIndex]
+        arrayOfChars[rightIndex] = temp 
+
+        leftIndex++
+        rightIndex--
+    }
+    console.log(arrayOfChars)
+  }
+
+  reverse(["a","b","c","d","f","g"])
