bb6fc5dec5049044cd84534a6143ff88e48c6473
Miles Higbie <miles.higbie@gmail.com>
2020-05-23 14:32:04 -0400

    testing process in node

---
+++ nodeJsTest.js
@@ -0,0 +1,16 @@
+let initialMemory = process.memoryUsage().heapUsed;
+let word = process.argv[2];
+let name = process.argv[3];
+
+console.log(`Your word is ${word} and your name is ${name}`)
+
+// Create a new array
+let wordArray = [];
+
+// Loop 1000 times, pushing into the array each time 
+for (let i = 0; i < 1000; i++){
+  wordArray.push(`${word} count: ${i}`)
+}
+
+console.log(`Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${process.memoryUsage().heapUsed}. \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
+
